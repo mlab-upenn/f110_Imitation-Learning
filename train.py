7f8d84abbd48f91a022178fe085d28551cce368e
botforge <botforge@github.com>
2019-07-07 11:41:29 -0400

    Done w/ train loop

--- train.py
+++ train.py
@@ -8,21 +8,35 @@ from data_utils import Data_Utils
 device = torch.device('cuda' if torch.cuda.is_available else 'cpu') 
 logger = logging.getLogger()
 
-def loss_pass(net, loss, train_loader, epoch, optimizer, train=True):
+def loss_pass(net, loss, loader, epoch, optimizer, train=True):
     """
-    Performs a pass over all inputs in this epoch
+    Performs loss pass over all inputs in this epoch
     """
+
     if not train:
         torch.set_grad_enabled(False)
+        total_epoch_loss = 0
+        for i, data in enumerate(loader):
+            img_tensor, angle_true = data
         
-        
+            #Classic train loop
+            optimizer.zero_grad()
+            angle_pred = net(img_tensor)
+            loss_tensor = loss(angle_pred, angle_true)
+            if train:
+                loss_tensor.backward()
+                optimizer.step()
+            logger.info("loss:{}".format(loss_tensor.item()))
+            total_epoch_loss += loss_tensor.item()
     if not train:
         torch.set_grad_enabled(True)
+    return total_epoch_loss
 
 def train(net, num_epochs, optimizer, loss_func, train_loader, valid_loader):
     for epoch in range(0, num_epochs):
         logger.info("Starting epoch: {}".format(epoch))
-        train_loss = loss_pass(net, loss_func, train_loader, epoch, optimizer, train=True)
+        train_epoch_loss = loss_pass(net, loss_func, train_loader, epoch, optimizer, train=True)
+        valid_epoch_loss = loss_pass(net, loss_func, valid_epoch_loss, epoch, optimizer, train=False)
 
 def main():
     #Set random seeds
