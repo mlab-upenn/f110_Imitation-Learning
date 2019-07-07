f4e57fca5737cf28a3e5c5c44b6f97f379510511
botforge <botforge@github.com>
2019-07-07 18:05:45 -0400

    added logging functionality

--- train.py
+++ train.py
@@ -5,7 +5,11 @@ import numpy as np
 from models import NVIDIA_ConvNet
 from tensorboardX import SummaryWriter
 from data_utils import Data_Utils
+from datetime import datetime
+
 device = torch.device('cuda' if torch.cuda.is_available else 'cpu') 
+now = datetime.now()
+date_time = now.strftime("_%H:%M:%S_%m-%d-%Y")
 
 def loss_pass(net, loss, loader, epoch, optimizer, train=True):
     """
@@ -37,18 +41,28 @@ def loss_pass(net, loss, loader, epoch, optimizer, train=True):
     return total_epoch_loss
 
 def train(net, num_epochs, optimizer, loss_func, train_loader, valid_loader):
-    writer = SummaryWriter("__tblogs")
+    global date_time
+    best_train_loss = float('inf')
+    best_valid_loss = float('inf')
+    writer = SummaryWriter("__runs/" + str(date_time) + '/')
     for epoch in range(0, num_epochs):
         print("Starting epoch: {}".format(epoch))
         train_epoch_loss = loss_pass(net, loss_func, train_loader, epoch, optimizer, train=True)
         valid_epoch_loss = loss_pass(net, loss_func, valid_loader, epoch, optimizer, train=False)
+
+        #Logging & Saving stuff
         print("----------------EPOCH{}STATS:".format(epoch))
         print("TRAIN LOSS:{}".format(train_epoch_loss))
         print("VALIDATION LOSS:{}".format(valid_epoch_loss))
         print("----------------------------")
-        writer.add_scalar('Train Loss', train_epoch_loss)
-        writer.add_scalar('Valid Loss', valid_epoch_loss)
+        writer.add_scalar('Train Loss', train_epoch_loss, epoch)
+        writer.add_scalar('Valid Loss', valid_epoch_loss, epoch)
+        if best_train_loss > train_epoch_loss:
+            torch.save(net.state_dict(), "__runs/" + str(date_time) + '/best_train_model')
+        if best_valid_loss > valid_epoch_loss:
+            torch.save(net.state_dict(), "__runs/" + str(date_time) + '/best_valid_model')
     writer.close()
+
 def main():
     #Set random seeds
     seed = 6582
@@ -62,7 +76,7 @@ def main():
     net = NVIDIA_ConvNet().to(device)
     loss_func = nn.functional.mse_loss
     optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
-    num_epochs = 1
+    num_epochs = 1000
     batch_size = 64
     
     #Make Dataloaders
