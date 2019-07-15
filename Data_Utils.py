e3fac0c82121a4986bf73f533574fe235f0fc3d4
botforge <botforge@github.com>
2019-07-15 14:50:11 -0400

    done w data utils

--- Data_Utils.py
+++ Data_Utils.py
@@ -53,8 +53,41 @@ class Data_Utils(object):
             func = self.tf_partial(tf["F"], tf["args"])
             parsed_tf_list.append(func)
         return parsed_tf_list
+    
+    def apply_tfs(self, old_img, old_row, tfs):
+        new_img = old_img
+        new_row = old_row
+        for tf in tfs:
+            new_img, new_row = tf(new_img, new_row)
+        return new_img, new_row
 
     def preprocess_folder(self, sourcepath, destpath, tf_list):
-        parsed_tf_list = self.get_partials_list(tf_list)
+        "TODO: Account for when destpath already exists"
+        tfs = self.get_partials_list(tf_list)
 
+        #make directories to destpath
+        os.makedirs(destpath)
+
+        #set up new csv file/dataframe
         csvpath = os.path.join(sourcepath, "data.csv")
+        old_df = pd.read_csv(csvpath)
+        col_names = old_df.columns.values
+        new_df = pd.DataFrame(columns=col_names)
+
+        for i in range(len(old_df)):
+            #get old_image and old_row
+            old_row = old_df.iloc[i]
+            img_name = old_row[0]
+            img_path = os.path.join(sourcepath, img_name)
+            old_img = cv2.imread(img_path)
+
+            #apply operations to get new_img and new_row
+            new_img, new_row = self.apply_tfs(old_img, old_row, tfs)
+
+            #write image and append to new_df
+            new_img_path = os.path.join(destpath, img_name)
+            cv2.imwrite(new_img_path, new_img)
+            new_df.append(new_row)
+        
+        newcsvpath = os.path.join(new_img_path)
+        new_df.to_csv(newcsvpath)
