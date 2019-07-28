96c975b06cae1a664796ad5973372f52fc0af19d
botforge <botforge@github.com>
2019-07-28 06:26:57 -0400

    small bug fixed

--- src/nnet/__init__.py
+++ src/nnet/__init__.py
@@ -1,7 +1,2 @@
 import os, sys
-from nnet.Metric_Visualizer import Metric_Visualizer
-from nnet.Data_Utils import Data_Utils
-from nnet.func_utils import *
-from nnet.Stepper import Stepper
-from nnet.Online import Online
-from nnet.train import Trainer
+sys.path.append(os.getcwd())
