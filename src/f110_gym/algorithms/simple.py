2eb987a6a35faee43978d70cf42085826f7fde0d
botforge <dhruvkar@seas.upenn.edu>
2019-07-28 13:37:51 -0400

    modified simplepy to look nicer

--- src/f110_gym/algorithms/simple.py
+++ src/f110_gym/algorithms/simple.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 from f110_gym.wrappers.imitation_wrapper import make_imitation_env
 from f110_gym.f110_core import f110Env
-import rospy
+import rospy, cv2
 import threading
 import numpy as np
 
@@ -10,9 +10,11 @@ __author__ = 'Dhruv Karthik <dhruvkar@seas.upenn.edu>'
 
 env = f110Env()
 obs = env.reset()
-for i in range(1000):
+while True:
     random_action = {"angle":0.2, "speed":1.0}
     obs, reward, done, info = env.step(random_action)
-    print(obs.keys())
+    cv_img = obs["img"]
+    cv2.imshow('latestimg', cv_img)
+    cv2.waitKey(2)
     if done:
         obs = env.reset() 
