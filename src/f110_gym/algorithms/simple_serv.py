2e3cb34f049a8dfc962239b84b7cad571ed79f06
botforge <dhruvkar@seas.upenn.edu>
2019-07-29 13:32:32 -0400

    added deserialization in exp_server

--- src/f110_gym/algorithms/simple_serv.py
+++ src/f110_gym/algorithms/simple_serv.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 from f110_gym.wrappers.imitation_wrapper import make_imitation_env
 from f110_gym.f110_core import f110env
+from distributed.exp_sender import ExperienceSender
 import rospy, cv2
 import threading
 import numpy as np
@@ -9,12 +10,3 @@ import numpy as np
 __author__ = 'dhruv karthik <dhruvkar@seas.upenn.edu>'
 
 env = f110env()
-obs = env.reset()
-for i in range(100):
-    random_action = {"angle":0.2, "speed":1.0}
-    obs, reward, done, info = env.step(random_action)
-    cv_img = obs["img"]
-    cv2.imshow('latestimg', cv_img)
-    cv2.waitkey(2)
-    if done:
-        obs = env.reset() 
