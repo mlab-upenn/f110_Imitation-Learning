884236f199d4e9cabe31f6aa97f83dbf3b1562b1
botforge <dhruvkar@seas.upenn.edu>
2019-07-30 07:04:18 -0400

    changed server to log info on sending back nn

--- src/distributed/exp_server.py
+++ src/distributed/exp_server.py
@@ -35,4 +35,5 @@ class ExperienceServer(threading.Thread):
             obs_array = self.dump_to_obs(msg_dump_arr[2:])
             reply_dump_array = self.recv_callback(obs_array)
             dump_array = [msg_dump_arr[0], msg_dump_arr[1]] + reply_dump_array
+            print('SENT BATCH:', header_dict.get("batchnum"))
             self.zmq_socket.send_multipart(dump_array)
