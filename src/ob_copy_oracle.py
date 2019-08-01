ab45735bc52aead4f82fe910d0657d58a149c2c6
botforge <dhruvkar@seas.upenn.edu>
2019-08-01 05:42:42 -0400

    done with copy_oracle

--- src/ob_copy_oracle.py
+++ src/ob_copy_oracle.py
@@ -19,14 +19,63 @@ __author__ = 'Dhruv karthik <dhruvkar@seas.upenn.edu>'
 
 class Copy_Oracle(object):
     """
-    Enables expert policy execution for collecting training data
+    Copies and executes expert policy while frequently sending batches over to the server
     """
     def __init__(self):
         self.serv_sender = ExperienceSender()
         self.record = False
         self.env = make_imitation_env()
+        self.oracle = FGM()
         
-        #this is where we store observations for sender
+        #store observations for sender (sending off to server for training)
         self.sender_buffer = deque(maxlen=20)
 
+    def get_action(self, obs_dict):
+        """ Gets action from self.oracle returns action_dict for gym"""
+        ret_dict = self.oracle.fix(obs_dict)
+        return ret_dict
+
+    def run_policy(self):
+        """ Uses self.oracle to run the policy onboard"""
+        env = make_imitation_env(skip=2)
+        obs_dict = env.reset()
+        self.sender_buffer.append(obs_dict)
+        while True:
+            action = self.get_action(obs_dict)
+            nobs_dict, reward, done, info = env.step(action)
+            if info.get("record"):
+                self.sender_buffer.append(nobs_dict)
+            obs_dict = nobs_dict
+            if done:
+                obs_dict = env.reset()
     
+    def server_callback(self, reply_dump):
+        """When the server returns something, this function will get called from another thread"""
+        print("YEET")
+        pass
+
+    def send_batches(self):
+        """Handles sending batches of experiences sampled from the replay buffer to the Server for training """
+        while True:
+            if len(self.sender_buffer) > 21:
+                obs_array = []
+                for i in range(20):
+                    #FYI:popping from opposite side of deque is thread-safe
+                    obs_array.append(self.sender_buffer.popleft())
+                self.serv_sender.send_obs(obs_array, self.env.serialize_obs(), self.server_callback)
+            time.sleep(10)
+
+def main():
+    co = Copy_Oracle()
+    server_thread = threading.Thread(target=co.send_batches)
+    server_thread.daemon = True
+    server_thread.start() #run recording on another thread
+    co.run_policy() #run policy on the main thread
+    server_thread.join()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        rospy.signal_shutdown('Done')
+        pass
