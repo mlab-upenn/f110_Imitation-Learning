02edf15d19f6896329bbdc7b7fc15f2051a570b6
botforge <dhruvkar@seas.upenn.edu>
2019-08-02 08:05:12 -0400

    many changes, added augmentations

--- src/common/augs.py
+++ src/common/augs.py
@@ -4,3 +4,12 @@ def mirror(obs_dict):
     Expects cv_img input
     """
     return obs_dict
+
+def toDeg():
+    import math
+    def call(obs_dict):
+        steer = obs_dict["steer"]
+        angle_deg = steer["angle"] * 180.0/math.pi
+        steer["angle"] = angle_deg
+        return obs_dict
+    return call
