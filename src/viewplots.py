0d0f8154d9e4fe691dec2de29f6d755fc2d7e421
botforge <dhruvkar@seas.upenn.edu>
2019-12-10 15:47:04 -0500

    dogs

--- src/viewplots.py
+++ src/viewplots.py
@@ -6,8 +6,11 @@ global_returns = []
 with open("out_returns.pkl", 'rb') as f:
     global_returns = pickle.load(f)
 
-plt.plot(global_returns)
+plt.plot(global_returns, c='purple')
 for i in range(int(len(global_returns)/3)):
-    plt.axvline(x=i*3, c='orange', ls='--')
+    plt.axvline(x=i*3, c='orange', ls=':')
 
+plt.legend(['Reward Curve', 'Dagger Rollout'])
+plt.xlabel("Episode")
+plt.ylabel("Reward / Time until crash")
 plt.show(block=True)
